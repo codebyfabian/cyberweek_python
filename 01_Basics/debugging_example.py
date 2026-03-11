57bc98206496feacba5b24dcf87fdd36b9a3f774
TheKingFabi2002 <fabian-land@web.de>
2026-03-11 09:48:04 +0100

    added files from the second day of the course

---
+++ 01_Basics/debugging_example.py
@@ -0,0 +1,9 @@
+print("Hello World")
+print("Hello Hello Esslingen")
+
+dic_test=["key1:", "value1:", "key2:", "value2:", "key3:", 15]
+if "kwd1" in dic_test:
+    print(dic_test["kwd1"])
+if "kwd2" in dic_test:
+    print(dic_test["kwd2"])
+print(dic_test)
